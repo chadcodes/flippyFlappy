55be7192dc1074b0e31e5254c85899333976b7fb
Gavin Thompson <gavinathompson@gmail.com>
2015-05-20 10:35:32 -0400

    adding bird to scene

--- flippyFlappy/GameScene.swift
+++ flippyFlappy/GameScene.swift
@@ -9,14 +9,19 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var bird = SKSpriteNode()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        var birdTexture = SKTexture( imageNamed: "img/flappy1.png" )
+        
+        bird = SKSpriteNode( texture: birdTexture )
+        bird.position = CGPoint( x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame) )
+        
+        self.addChild( bird )
+      
     }
     
     
@@ -26,17 +31,6 @@ class GameScene: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
